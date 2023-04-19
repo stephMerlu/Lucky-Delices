df285d262bc4268d8a31d0d6306b7604071f75b1
stephMerlu <stephanie.merluzzo@gmail.com>
2023-04-19 17:22:06 +0200

    crud user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -128,4 +128,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->roles;
+    }
 }
